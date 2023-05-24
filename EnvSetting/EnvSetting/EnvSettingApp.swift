542e2139cfe57cd41dfd0d25debc70700aec0fb4
Bran <cold929@naver.com>
2023-05-24 16:59:21 +0900

    chore: project init

---
+++ EnvSetting/EnvSetting/EnvSettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvSettingApp.swift
+//  EnvSetting
+//
+//  Created by Bran on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EnvSettingApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
