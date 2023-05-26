f1fc89e7d812b1c49b011be180d4652ad805ca39
Bran <cold929@naver.com>
2023-05-26 15:58:36 +0900

    feat: set custom flag

--- EnvSetting/EnvSetting/ContentView.swift
+++ EnvSetting/EnvSetting/ContentView.swift
@@ -13,7 +13,14 @@ struct ContentView: View {
       Image(systemName: "globe")
         .imageScale(.large)
         .foregroundColor(.accentColor)
+
+      #if DEV
+      Text("Hello, DEV world!")
+      #elseif STAGE
+      Text("Hello, STAGE world!")
+      #else
       Text("Hello, world!")
+      #endif
     }
     .padding()
   }
